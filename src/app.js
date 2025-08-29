3d6e2bfa43bfbc89d0b2d23696c1e3222185fb54
ashu2764 <itsashu268@gmail.com>
2025-08-29 23:46:07 +0530

    Practice of middleware and handling errors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,36 @@
 import express from 'express';
+import { adminAuthMiddleware } from './middleware/adminAuth.middleware.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/test", (req, res)=>{
-    res.send("Hello World! This is DevTinder");
+app.use('/admin', adminAuthMiddleware);
+
+// app.use("/test", (req, res)=>{
+//     res.send("Hello World! This is DevTinder");
+// })
+
+app.get('/admin/login', (req, res)=>{
+    try {
+
+        console.log("Admin login route accessed");
+         res.send("Admin Login Successful");
+        
+    } catch (error) {
+        console.error("Error during admin login:", error);
+        res.status(500).send("Internal Server Error");
+    }
+   
 })
 
 app.get("/hello", (req, res)=>{
     res.send("Welcome to DevTinder");   
 })
+
+app.use('/', (err, req,res,next) =>{
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
---
+++ src/middleware/adminAuth.middleware.js
@@ -0,0 +1,9 @@
+export const adminAuthMiddleware = (req, res, next) => {
+    const token  = "123456"
+    const isAdminAuthenticated = token === "123456"; // Example check
+    if(!isAdminAuthenticated){
+        return res.status(403).send("Forbidden: Admins only");
+    }else{
+        next();
+    }
+}
